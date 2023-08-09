b2af40e477050e93e3d600a4586bd2accf1351e4
Frank Alimimian <alimimianfrank@gmail.com>
2023-08-09 21:24:27 +0100

    Add navigation section

--- main/views.py
+++ main/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("<h1>Hello world<h1>")
+    return render(request, 'main/index.html', {})
