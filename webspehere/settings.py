a5e21717cafac8191dd921101c137644e5d00d24
Frank Alimimian <alimimianfrank@gmail.com>
2024-07-23 19:26:31 +0100

    minor update

--- webspehere/settings.py
+++ webspehere/settings.py
@@ -222,9 +222,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER = "Frankyngood@gmail.com"
-EMAIL_HOST_PASSWORD = "qmtz mjvp avwy chef"
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 ACCOUNT_EMAIL_VERIFICATION = "none"
 
